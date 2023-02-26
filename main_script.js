f9ac081a646852ace7d894f5883027c0f3920eb0
TokaiComputerClub <126393055+TokaiComputerClub@users.noreply.github.com>
2023-02-26 21:14:49 +0900

    Add files via upload

---
+++ main_script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#page-link a[href*="#"]').click(function () {
+        var elmHash = $(this).attr('href');
+        var pos = $(elmHash).offset().top;
+        $('body,html').animate({scrollTop: pos}, 500);
+        return false;
+    });
+    
+    $('.slider').slick({
+        autoplay: true
+    });
+})
