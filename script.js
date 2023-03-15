7051543a6e86905f1364c01927e489e28b480b7c
TokaiComputerClub <tokai.computerClub@gmail.com>
2023-03-15 21:47:49 +0900

    fix

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
 
     //スライダー
     $("#Slider").slick({
-        autoplay:true
+        autoplay:true,
+        autoplaySpeed:3000,
+        centerMode:true,
+        // centerPadding:50px,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+        speed:300,
     });
 
     //動画ブロック
     $("#see_video").on("click",() => {
         $("#block_video").css({"display":"none"});
     });
+
+    //質問ボタン
+    $("#question_button").on("click",() => {
+        $("#form_container").css({"display":"flex"})
+    })
 });
 
 
