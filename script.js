8baa5c4464e64266325efd87f122952baa31c56a
TokaiComputerClub <tokai.computerClub@gmail.com>
2023-03-21 12:28:31 +0900

    icon,image

--- script.js
+++ script.js
@@ -15,31 +15,4 @@ $(document).ready(function(){
     $("#see_video").on("click",() => {
         $("#block_video").css({"display":"none"});
     });
-
-    //質問ボタン
-    $("#question_button").on("click",() => {
-        $("#form_body").css({"display":"flex"})
-    })
-    //キャンセルボタン
-    $("#cancel").on("click",() => {
-        $("#form_body").css({"display":"none"})
-    })
-
-    //フォーム送信
-    $("#question_form").on("submit",() => {
-        const SERVICE_ID = "service_xx4tqrl"
-        const TEMPLATE_ID = "template_qd7651v"
-        const MESSAGE = {
-            student_number : $("student_number").text
-        }
-
-        emailjs.send(SERVICE_ID,TEMPLATE_ID,MESSAGE)
-        .then(function(response) {
-            alert('SUCCESS!', response.status, response.text);
-        }, function(error) {
-            alert('FAILED...', error);
-        });
-    })
 });
-
-
